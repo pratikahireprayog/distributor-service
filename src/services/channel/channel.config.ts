3b193f2b32a595c412897bf5ace92768dfac845e
Avinash Singh <avinash.singh@shreemaruti.com>
2025-02-26 16:32:55 +0530

    PLATFORM-215 cleanup

--- src/services/channel/channel.config.ts
+++
@@ -1,6 +0,0 @@
-import { ChannelTypeEnum } from 'src/common/enums';
-import { FynoChannel } from './implementations/fyno/fyno';
-
-export const CHANNEL_TYPE_TO_CLASS_MAP = {
-  [ChannelTypeEnum.FYNO]: FynoChannel,
-};
