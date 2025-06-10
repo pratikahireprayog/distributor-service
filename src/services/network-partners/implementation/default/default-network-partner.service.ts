5c28a9fb2fb0dea87455fe721ae295f38607e3a8
devyani <devyani.patil@shreemaruti.com>
2025-06-10 14:27:03 +0530

    add new field docType

--- src/services/network-partners/implementation/default/default-network-partner.service.ts
+++ src/services/network-partners/implementation/default/default-network-partner.service.ts
@@ -518,7 +518,7 @@ export class DefaultNetworkPartner extends BaseNetworkPartner {
         bookingType: order.type,
         // ewayBillCreateDate: null,
         ewayBillNumber: order?.ewayBillNos?.[0] || "",
-        documentType: order?.docType || "",
+        docType: order?.docType || "",
         // expiryDate: null,
         extendEwayBillCount: 0,
         fromPincode: parseInt(order?.pickupAddress?.zip),
