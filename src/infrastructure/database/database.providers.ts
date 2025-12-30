e3d58922584a5011e53e74fd77538fed438fd529
Arpan Mukherjee <arpan.mukherjee@hutechsolutions.com>
2025-12-30 16:24:41 +0530

    db config change

--- src/infrastructure/database/database.providers.ts
+++ src/infrastructure/database/database.providers.ts
@@ -12,8 +12,10 @@ export const databaseProviders = [
   {
     provide: RepositoryConst.DATABASE_NAME_CONST.DISTRIBUTOR_DB,
     useFactory: async (): Promise<mongoose.Connection> => {
-      const connection = await mongoose.createConnection(process.env.DISTRIBUTOR_DB_URL);
+      const connection = mongoose.createConnection(process.env.DISTRIBUTOR_DB_URL);
+      await connection.asPromise(); // 🔴 THIS IS THE FIX
       return connection;
+      
     },
   },
 ];
